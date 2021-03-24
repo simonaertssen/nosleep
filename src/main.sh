8e7d8d5743567162b9a54dea28b1e3acbb74f12b
Simon Aertssen <Simonaertssen@gmail.com>
2021-03-24 16:03:58 +0100

    Testing for change in main fuction

--- src/main.sh
+++ src/main.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source src/config.sh
+source src/get_system_info.sh
 
 # Log start of the program:
 log=logs/log_file.txt
@@ -7,20 +9,46 @@ echo "$(date) Program Start" >> $log
 # Trap CRTL+C to cleanup resources and reset the pmset settings to normal
 trap reset_settings SIGINT
 reset_settings() {
-	sudo pmset -b disablesleep 0
-    sudo pmset -b sleep $B_SLEEP
-    sudo pmset -b displaysleep 1
-    echo "$(date) Program Stop" >> $log
+	# sudo pmset -b disablesleep 0
+    # sudo pmset -b sleep $B_SLEEP
+    # sudo pmset -b displaysleep 1
+    echo "$(date) Program Stop"# >> $log
     exit
 }
+
+# Set default here to track change over time:
+lid_closed_before=false
+xtrnl_display_before=false
 
 # Main event loop:
 while :
 do
+	# Perform loop with some time off
+    sleep 1
 
-	# Check if in battery mode:
+	# Check if lid is closed:
+	lid_closed_now="$(is_lid_closed)"
+	if [ "$lid_closed_now" = false ]; then # Continue loop if lid is open
+		continue
+	fi
+	if [ "$lid_closed_now" == "$lid_closed_before" ]; then # Continue loop if no change occured
+		continue
+	fi
+	lid_closed_before=$lid_closed_now
+
+	echo "A change occured"
 
 	# Check if external display is connected
-    ((count++))
-    sleep 1
+	xtrnl_display_now="$(is_external_display_connected)"
+	if [ "$xtrnl_display_now" = false ]; then # Continue if there is no second display
+		continue
+	fi
+	if [ "$xtrnl_display_now" == "$xtrnl_display_before" ]; then # Continue if no change occured
+		continue
+	fi
+	xtrnl_display_before=$xtrnl_display_now
+
+	echo "A change occured"
+
+
 done
