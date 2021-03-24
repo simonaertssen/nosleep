1f9abcef625d43b5ac07611cf16572453f8fcd3c
Simon Aertssen <Simonaertssen@gmail.com>
2021-03-24 15:50:17 +0100

    Removed double comparison for strings

--- tests/test_system_info.sh
+++ tests/test_system_info.sh
@@ -4,7 +4,7 @@ source ./src/get_system_info.sh
 
 echo "Charger connected?"
 power_adapter_connected="$(is_power_adapter_connected)"
-if [ "$power_adapter_connected" == true ]; then
+if [ "$power_adapter_connected" = true ]; then
 	echo "Yes"
 else
 	echo "No"
@@ -18,7 +18,7 @@ echo $battery_level
 
 echo "External display?"
 external_display="$(is_external_display_connected)"
-if [ "$external_display" == true ]; then
+if [ "$external_display" = true ]; then
 	echo "Yes"
 else
 	echo "No"
@@ -27,7 +27,7 @@ fi
 
 echo "Lid closed?"
 lid_closed="$(is_lid_closed)"
-if [ "$lid_closed" == true ]; then
+if [ "$lid_closed" = true ]; then
 	echo "Yes"
 else
 	echo "No"
