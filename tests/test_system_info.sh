5b8bdf87b482d5179779a7d2f195dc3292ab8657
Simon Aertssen <Simonaertssen@gmail.com>
2021-03-24 15:24:47 +0100

    Tests working succesfully

--- tests/test_system_info.sh
+++ tests/test_system_info.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+source ./src/get_system_info.sh
 
-# Create log file or overrite if already present
-log=logs/log_file.txt
-echo "$(date) Log File Start ----- " > $log
+echo "Charger connected?"
+power_adapter_connected="$(is_power_adapter_connected)"
+if [ "$power_adapter_connected" == true ]; then
+	echo "Yes"
+else
+	echo "No"
+fi
+
+echo "Checking battery level:"
+battery_level="$(get_battery_level)"
+echo $battery_level
+
+echo "External display?"
+external_display="$(is_external_display_connected)"
+if [ "$external_display" == true ]; then
+	echo "Yes"
+else
+	echo "No"
+fi
