f6318e6c7db55b37d1f27ca04cfce6fe083972f0
Simon Aertssen <Simonaertssen@gmail.com>
2021-03-24 15:37:43 +0100

    System info seems to work

--- src/get_system_info.sh
+++ src/get_system_info.sh
@@ -11,35 +11,25 @@ is_power_adapter_connected() {
 }
 
 get_battery_level() {
-	echo "Checking battery level"
 	local battery_level=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 	echo "$battery_level"
 }
 
 is_external_display_connected() {
-	echo "Checking number of displays"
 	local external_display=false
 	if [[ $(system_profiler SPDisplaysDataType | grep -c Resolution) -gt 1 ]]
 	then
 		external_display=true
 	fi
-	echo $(system_profiler SPDisplaysDataType | grep -c Resolution)
 	echo "$external_display"
 }
 
-power_adapter_connected="$(is_power_adapter_connected)"
-if [ "$power_adapter_connected" == true ]; then
-	echo "Connected"
-else
-	echo "Not connected"
-fi
-
-battery_level="$(get_battery_level)"
-echo $battery_level
-
-external_display="$(is_external_display_connected)"
-if [ "$external_display" == true ]; then
-	echo "External Display"
-else
-	echo "No External Display"
-fi
+is_lid_closed() {
+	local lid_closed=true
+	OUTPUT=$(ioreg -r -k AppleClamshellState | grep '"AppleClamshellState"' | cut -f2 -d"=")
+	if [[ "$OUTPUT" == " No" ]]
+	then
+		lid_closed=false
+	fi
+	echo "$lid_closed"
+}
