3402432f6da523aa71c918b60f1ce3e3099cc3fb
Simon Aertssen <Simonaertssen@gmail.com>
2021-03-24 15:37:27 +0100

    Tests seem to work fine

--- tests/test_system_info.sh
+++ tests/test_system_info.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source ./src/get_system_info.sh
 
+
 echo "Charger connected?"
 power_adapter_connected="$(is_power_adapter_connected)"
 if [ "$power_adapter_connected" == true ]; then
@@ -9,13 +10,24 @@ else
 	echo "No"
 fi
 
-echo "Checking battery level:"
+
+echo "Checking battery level?"
 battery_level="$(get_battery_level)"
 echo $battery_level
+
 
 echo "External display?"
 external_display="$(is_external_display_connected)"
 if [ "$external_display" == true ]; then
+	echo "Yes"
+else
+	echo "No"
+fi
+
+
+echo "Lid closed?"
+lid_closed="$(is_lid_closed)"
+if [ "$lid_closed" == true ]; then
 	echo "Yes"
 else
 	echo "No"
